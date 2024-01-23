a79e6403b9f58ac5fb913f7d6d1646d265202723
Hissitissam <tissam.hisssi@gmail.com>
2024-01-23 03:31:37 -0800

    siwe square  class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(abc, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): Size of the new square.
+        """
+        abc.__size = size
